79c636c436767ff9540280df01137024bc0cff25
mavHo <ho.maverick@gmail.com>
2019-03-26 13:36:22 -0700

    endstats is now a dict, added future live events funct

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 from flask import Flask, jsonify, abort, make_response, request, url_for
 from bs4 import BeautifulSoup
 import urllib.request
+import sys
 
 #need lxml parser for accurate results
     
 
 class UFCParser():
 
-    def getRawHTML(url):
+    def getRawHTML(self,url):
         endpoint = urllib.request.urlopen(url) 
         mybytes = endpoint.read()
         endpoint.close()
@@ -16,20 +17,32 @@ class UFCParser():
     #this gets the first event link within the event list
     #future funct: needs to determine whether or not it is live 
     #success boolean? maybe do check later on
-    def getLiveEvent(data):
+    def getLiveEvent(self,data):
         soup = BeautifulSoup(data, "lxml")
         event_url = (soup.find('ul', {'class':'l-listing__group'})).find('li')
         return (event_url.find('a', href=True)['href'])
 
+    def getliveFightURL(self, data):
+        event_json = [] 
+        soup = BeautifulSoup(data, "lxml")
+        #find the live fight
+        #open another page for it...
+        print('hello')
+        return
+    def dumpStats(self, data):
+        end_stats = {}
+        end_stats['Time'] = data.find('div', {'class':'c-listing-fight__result-text time'}).get_text()
+        end_stats['Round'] = data.find('div', {'class':'c-listing-fight__result-text round'}).get_text()
+        end_stats['Method'] = data.find('div', {'class':'c-listing-fight__result-text method'}).get_text()
 
+        return end_stats
     #given raw_html of some event page, parse it
-    def parseEvent(data):
+    def parseEvent(self, data):
         event_json = [] 
         soup = BeautifulSoup(data, "lxml")
 
         for listing in soup.find_all('div', {'class':'c-listing-fight'}):
             fight = {}
-
 
             red_fighter = listing.find('div', {'class':'c-listing-fight__corner--red'})
             blue_fighter = listing.find('div', {'class':'c-listing-fight__corner--blue'})
@@ -48,17 +61,19 @@ class UFCParser():
                 fight['Winner'] = 'unkown'
 
                 
-            weight_class = (listing.find('div', {'class': 'c-listing-fight__class'}))
+            weight_class = listing.find('div', {'class': 'c-listing-fight__class'})
             end_stats = listing.find('div', {'class':'js-listing-fight__results'})
 
+            fight['end_stats'] = self.dumpStats(end_stats)
             fight['weight-class'] = weight_class.get_text(strip=True)
             fight['red-corner'] = red_name.get_text(strip=True)
             fight['blue-corner'] = blue_name.get_text(strip=True)
-            fight['end_stats'] = end_stats.get_text(strip=True)
             #fight['data-fmid'] = listing['data-fmid']
             event_json.append(fight)
 
         return event_json
+
+
 
 class ConfigURL:
     url = "https://www.ufc.com/events"
@@ -67,14 +82,29 @@ class ConfigURL:
 class API():
     app = Flask(__name__)
 
-    @app.route('/ufc/api/v1.0/events/all', methods=['GET'])
-    def get_live_event():
+    @app.route('/ufc/api/v1.0/live-events/current', methods=['GET'])
+    def get_live_fight():
+        configobj = ConfigURL()
+        parser = UFCParser()
+        raw_html = parser.getRawHTML(configobj.url) 
+
+        event = parser.getLiveEvent(raw_html)
+
+        live_html = parser.getRawHTML("https://www.ufc.com/"+ event)
+        live_fight_url = parser.getliveFightURL(live_html) 
+        live_results = parser.liveFight(parser.getRawHTML(live_fight_url)) 
+        return jsonify({'fights': live_results})
+
+    @app.route('/ufc/api/v1.0/live-events/all', methods=['GET'])
+    def get_all_events():
         #we start with the initial URL
         configobj = ConfigURL()
-        raw_html = UFCParser.getRawHTML(configobj.url) 
-        event = UFCParser.getLiveEvent(raw_html)
-        live_html = UFCParser.getRawHTML("https://www.ufc.com/"+ event)
-        live_results = UFCParser.parseEvent(live_html) 
+        parser = UFCParser()
+        raw_html = parser.getRawHTML(configobj.url) 
+        event = parser.getLiveEvent(raw_html)
+
+        live_html = parser.getRawHTML("https://www.ufc.com/"+ event)
+        live_results = parser.parseEvent(live_html) 
         return jsonify({'fights': live_results})
 
 
