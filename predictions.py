8e85e19c69bd2d3d60c0ad31c0e26ca98eaf7eb3
mavHo <homaverick@gmail.com>
2019-11-05 23:38:49 -0800

    predicted fighters have avg's computed based on past fight history

--- predictions.py
+++ predictions.py
@@ -3,6 +3,7 @@ from config import Config
 from sqlalchemy import create_engine
 from models import Events, Bouts
 import pandas as pd
+import sys
 
 #sklearn modules
 from sklearn.model_selection import train_test_split
@@ -11,6 +12,114 @@ from sklearn import metrics
 
 def p2f(str):
     return(float(str.strip('%'))/100)
+
+def populate_dataframes(data_frame, in_fighters, blue_fighter, red_fighter):
+    #vars to calculate avgs of respective fighters
+    avg_rsigstr = avg_rsigstr_prct = avg_rTD_prct = avg_rTD = avg_rKD = avg_rpass = avg_rsub = avg_rttlstr = 0 
+    avg_bsigstr = avg_bsigstr_prct = avg_bTD_prct = avg_bTD = avg_bKD = avg_bpass = avg_bsub = avg_bttlstr = 0 
+    #count how many fights each fighter had
+    red_cnt = 0
+    blue_cnt = 0
+
+    for index, rows in data_frame.iterrows():
+        if(rows['winner'] == rows['blue_fighter']):
+            data_frame.at[index, 'b_win']=int(1)
+            data_frame.at[index, 'r_win']=int(0)
+        elif(rows['winner'] == rows['red_fighter']):
+            data_frame.at[index, 'b_win']=int(0)
+            data_frame.at[index, 'r_win']=int(1)
+        else:
+            data_frame.at[index, 'b_win']=int(0)
+            data_frame.at[index, 'r_win']=int(0)
+
+        #local vars for the loop, don't wanna calculate every time
+        TD=0
+        try:
+            TD = int(rows['r_TD'].split('/')[1])
+        except (ValueError, IndexError):
+            TD = int(rows['r_TD'].split('of')[1])
+        TD_PRCT = p2f(rows['r_TD_PRCT'])
+        TTLSTR = int(rows['r_TTLSTR'].split('/')[1])
+        TTLSTR_PRCT = float(int(rows['r_TTLSTR'].split('/')[0])/int(rows['r_TTLSTR'].split('/')[1]))
+        SIGSTR = int(rows['r_SIGSTR'].split('/')[1])
+        SIGSTR_PRCT = p2f(rows['r_SIGSTR_PRCT'])
+
+        data_frame.at[index,'r_TD']=TD
+        data_frame.at[index,'r_TTLSTR']=TTLSTR
+        data_frame.at[index,'r_TTLSTR_PRCT']=TTLSTR_PRCT
+        data_frame.at[index,'r_SIGSTR']=SIGSTR
+        data_frame.at[index,'r_SIGSTR_PRCT']=SIGSTR_PRCT
+        data_frame.at[index,'r_TD_PRCT']=TD_PRCT
+
+        if rows['red_fighter'] == red_fighter:
+            avg_rTD += TD
+            avg_rsigstr += SIGSTR
+            avg_rsigstr_prct += SIGSTR_PRCT
+            avg_rTD_prct += TD_PRCT 
+            avg_rKD += rows['r_KD']
+            avg_rpass += rows['r_PASS']
+            avg_rsub += rows['r_SUB']
+            avg_rttlstr += TTLSTR  
+            red_cnt +=1
+
+        if rows['red_fighter'] == blue_fighter:
+            avg_bTD += TD
+            avg_bsigstr += SIGSTR
+            avg_bsigstr_prct += SIGSTR_PRCT
+            avg_bTD_prct += TD_PRCT 
+            avg_bKD += rows['b_KD']
+            avg_bpass += rows['b_PASS']
+            avg_bsub += rows['b_SUB']
+            avg_bttlstr += TTLSTR  
+            blue_cnt+=1
+
+        #################Blue side#################################################    
+        TD=0
+        try:
+            TD = int(rows['b_TD'].split('/')[1])
+        except (ValueError, IndexError):
+            TD = int(rows['b_TD'].split('of')[1])
+        TD_PRCT = p2f(rows['b_TD_PRCT'])
+        TTLSTR = int(rows['b_TTLSTR'].split('/')[1])
+        TTLSTR_PRCT = float(int(rows['b_TTLSTR'].split('/')[0])/int(rows['b_TTLSTR'].split('/')[1]))
+        SIGSTR = int(rows['b_SIGSTR'].split('/')[1])
+        SIGSTR_PRCT = p2f(rows['b_SIGSTR_PRCT'])
+
+        data_frame.at[index,'b_TD'] = TD
+        data_frame.at[index,'b_TTLSTR'] = TTLSTR
+        data_frame.at[index,'b_TTLSTR_PRCT']=TTLSTR_PRCT
+        data_frame.at[index,'b_SIGSTR']=SIGSTR
+        data_frame.at[index,'b_SIGSTR_PRCT']=SIGSTR_PRCT
+        data_frame.at[index,'b_TD_PRCT']=TD_PRCT
+
+        #correspond fighter's info throughout different dataframes
+        if rows['blue_fighter'] == blue_fighter:
+            avg_bTD += TD
+            avg_bsigstr += SIGSTR
+            avg_bsigstr_prct += SIGSTR_PRCT
+            avg_bTD_prct += TD_PRCT 
+            avg_bKD += rows['b_KD']
+            avg_bpass += rows['b_PASS']
+            avg_bsub += rows['b_SUB']
+            avg_bttlstr += TTLSTR  
+            blue_cnt+=1
+        if rows['blue_fighter'] == red_fighter:
+            avg_rTD += TD
+            avg_rsigstr += SIGSTR
+            avg_rsigstr_prct += SIGSTR_PRCT
+            avg_rTD_prct += TD_PRCT 
+            avg_rKD += rows['r_KD']
+            avg_rpass += rows['r_PASS']
+            avg_rsub += rows['r_SUB']
+            avg_rttlstr += TTLSTR  
+            red_cnt +=1
+    return in_fighters.append({'blue_fighter':blue_fighter,'b_KD':avg_bKD/blue_cnt, 'b_PASS':avg_bpass/blue_cnt,
+     'b_SIGSTR':avg_bsigstr/blue_cnt, 'b_SIGSTR_PRCT':avg_bsigstr_prct/blue_cnt
+        , 'b_SUB':avg_bsub/blue_cnt, 'b_TD': avg_bTD/blue_cnt, 'b_TD_PRCT':avg_bTD_prct/blue_cnt
+        ,'red_fighter':red_fighter,'r_KD':avg_rKD/red_cnt, 'r_PASS':avg_rpass/red_cnt,
+         'r_SIGSTR':avg_rsigstr/red_cnt, 'r_SIGSTR_PRCT':avg_rsigstr_prct/red_cnt, 'r_SUB':avg_rsub/red_cnt,
+          'r_TD':avg_rTD/red_cnt, 'r_TD_PRCT':avg_rTD_prct/red_cnt},ignore_index=True)
+
 
 def main():
     #db i'm accessing
@@ -23,11 +132,6 @@ def main():
     red_payload = 'red_fighter, r_KD, r_PASS, r_SIGSTR, r_SIGSTR_PRCT, r_SUB, r_TD, r_TD_PRCT, r_TTLSTR'
     stats_payload = 'winner, loser, end_round, time, method, result'
 
-    #red_query = "SELECT * FROM bouts WHERE red_fighter='Robert Whittaker'" 
-    red_query = "SELECT " + red_payload + " FROM bouts WHERE weight_class='" + weight_class+ "'" 
-    blue_query = "SELECT " + blue_payload + " FROM bouts WHERE weight_class='" + weight_class + "'" 
-    #blue_query = "SELECT * FROM bouts WHERE blue_fighter='Robert Whittaker'"
-
     ###
     #currently logistic regression doesn't distinguish on who the fighter is
     #there's no concept or reach, winstreak, or anything
@@ -35,50 +139,42 @@ def main():
     #furthermore data frames will be created based on the fighter as well as the opponent.
     ###
     #data_frame = pd.read_sql(red_query + ' UNION ' + blue_query + ' UNION ' + stats_payload, connection)
-    query = "SELECT * FROM bouts WHERE weight_class='" + weight_class +"'"
-    data_frame = pd.read_sql(query, connection)
+    blue_fighter = 'Gregor Gillespie'
+    red_fighter = 'Kevin Lee'
+
+
+    ###dataframe for fighter averages in the predict list
+    in_fighters = pd.DataFrame(columns=['blue_fighter','b_KD', 'b_PASS', 'b_SIGSTR', 'b_SIGSTR_PRCT', 'b_SUB', 'b_TD', 'b_TD_PRCT'
+            ,'red_fighter','r_KD', 'r_PASS', 'r_SIGSTR', 'r_SIGSTR_PRCT', 'r_SUB', 'r_TD', 'r_TD_PRCT'])
+#    in_fighters.at[0, 'blue_fighter']=blue_fighter
+#    in_fighters.at[0, 'red_fighter']=red_fighter
+            
+    #query for weight class
+    weight_query = "SELECT * FROM bouts WHERE weight_class='" + weight_class +"'"
+
+    #queries for the fighters in some bout
+    blue_fighter_query = "SELECT * FROM bouts WHERE blue_fighter='" + blue_fighter +"' or red_fighter='" + blue_fighter + "'"
+    red_fighter_query = "SELECT * FROM bouts WHERE blue_fighter='" + red_fighter +"' or red_fighter='" + red_fighter + "'"
+
+    union_query = blue_fighter_query + ' UNION ' + red_fighter_query
+
+
+    data_frame = pd.read_sql(union_query, connection)
+
     data_frame["b_win"] = 0;
     data_frame["r_win"] = 0;
-    for index, rows in data_frame.iterrows():
-        if(rows['winner'] == rows['blue_fighter']):
-            data_frame.set_value(index, 'b_win', int(1))
-            data_frame.set_value(index, 'r_win', int(0))
-        elif(rows['winner'] == rows['red_fighter']):
-            data_frame.set_value(index, 'b_win', int(0))
-            data_frame.set_value(index, 'r_win', int(1))
-        else:
-            data_frame.set_value(index, 'b_win', int(0))
-            data_frame.set_value(index, 'r_win', int(0))
-
-        #convert the strings to floats
-        try:
-            data_frame.set_value(index,'r_TD',int(rows['r_TD'].split('/')[1]))
-        except (ValueError, IndexError):
-            data_frame.set_value(index,'r_TD',int(rows['r_TD'].split(' of ')[1]))
-
-        #data_frame.set_value(index,'r_TTLSTR',float(int(rows['r_TTLSTR'].split('/')[0])/int(rows['r_TTLSTR'].split('/')[1])))
-        data_frame.set_value(index,'r_SIGSTR',int(rows['r_SIGSTR'].split('/')[1]))
-
-        data_frame.set_value(index,'r_SIGSTR_PRCT',p2f(rows['r_SIGSTR_PRCT']))
-        data_frame.set_value(index,'r_TD_PRCT',p2f(rows['r_TD_PRCT']))
-
-        try:
-            data_frame.set_value(index,'b_TD',int(rows['b_TD'].split('/')[1]))
-        except (ValueError, IndexError):
-            data_frame.set_value(index,'b_TD',int(rows['b_TD'].split(' of ')[1]))
-
-        #data_frame.set_value(index,'b_TTLSTR',float(int(rows['b_TTLSTR'].split('/')[0])/int(rows['b_TTLSTR'].split('/')[1])))
-        data_frame.set_value(index,'b_SIGSTR',int(rows['b_SIGSTR'].split('/')[1]))
-
-        data_frame.set_value(index,'b_SIGSTR_PRCT',p2f(rows['b_SIGSTR_PRCT']))
-        data_frame.set_value(index,'b_TD_PRCT',p2f(rows['b_TD_PRCT']))
+    out_fighters = populate_dataframes(data_frame, in_fighters, blue_fighter, red_fighter)
     
+    print(data_frame)
+    print(out_fighters)
+    in_fighters._clear_item_cache()
 
     ##################################################################################################
 
-    feature_col = ['b_KD', 'b_PASS', 'b_SIGSTR', 'b_SIGSTR_PRCT', 'b_SUB', 'b_TD', 'b_TD_PRCT',
-            'r_KD', 'r_PASS', 'r_SIGSTR', 'r_SIGSTR_PRCT', 'r_SUB', 'r_TD', 'r_TD_PRCT']
+    feature_col = ['b_KD', 'b_PASS', 'b_SIGSTR', 'b_SIGSTR_PRCT', 'b_SUB', 'b_TD', 'b_TD_PRCT', 'b_TTLSTR', 'b_TTLSTR_PRCT'
+            'r_KD', 'r_PASS', 'r_SIGSTR', 'r_SIGSTR_PRCT', 'r_SUB', 'r_TD', 'r_TD_PRCT', 'r_TTLSTR', 'r_TTLSTR_PRCT']
 
+    sys.exit()
 
     X = data_frame[feature_col]
     y = data_frame.r_win
@@ -100,8 +196,16 @@ def main():
     print(y_pred)
     print(X_test)
 
+    count = 0;
     for index,rows in X_test.iterrows():
         print(data_frame.loc[index]['blue_fighter'] + ' : ' + data_frame.loc[index]['red_fighter'])
+        if y_pred[count] == 1:
+            print('winner: ' +data_frame.loc[index]['red_fighter'])
+        else:
+            print('winner: ' + data_frame.loc[index]['blue_fighter'])
+        print('actual winner: ' + data_frame.loc[index]['winner'])
+        count += 1
+
 
 
 
