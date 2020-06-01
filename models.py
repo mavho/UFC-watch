6e7c91ca100d614ed1329b8f3872710fd17d7760
mavHo <homaverick@gmail.com>
2020-05-31 17:27:46 -0700

    added event routes to the api

--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from ufc_api import db
+from ufc_api import db, ma
 #Event table
 class Events(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -41,3 +41,39 @@ class Bouts(db.Model):
     b_PASS= db.Column(db.Integer, index=True)
     r_PASS= db.Column(db.Integer, index=True)
 
+class EventSchema(ma.SQLAlchemySchema):
+    class Meta:
+        model = Events
+    id = ma.auto_field()
+    event = ma.auto_field()
+
+class BoutSchema(ma.SQLAlchemySchema):
+    class Meta:
+        model = Bouts
+    event_id = ma.auto_field()
+    weight_class = ma.auto_field()
+    red_fighter = ma.auto_field()
+    blue_fighter = ma.auto_field()
+    winner = ma.auto_field() 
+    loser =  ma.auto_field()
+    result = ma.auto_field() 
+    end_round = ma.auto_field() 
+    time = ma.auto_field()
+    method = ma.auto_field()
+
+    b_KD = ma.auto_field()
+    r_KD = ma.auto_field()
+    b_SIGSTR = ma.auto_field()
+    r_SIGSTR = ma.auto_field()
+    b_SIGSTR_PRCT = ma.auto_field()
+    r_SIGSTR_PRCT = ma.auto_field()
+    b_TTLSTR = ma.auto_field()
+    r_TTLSTR = ma.auto_field()
+    b_TD= ma.Str()
+    r_TD= ma.Str()
+    b_TD_PRCT = ma.auto_field()
+    r_TD_PRCT = ma.auto_field()
+    b_SUB= ma.Str()
+    r_SUB= ma.Str()
+    b_PASS= ma.Str()
+    r_PASS= ma.Str()
--- ufc_api.py
+++ ufc_api.py
@@ -1,35 +1,83 @@
 from flask import Flask, jsonify, abort, make_response, request, url_for
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api 
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_marshmallow import Marshmallow
 from config import Config, DevConfig, ProdConfig
 import os,sys, json
 
 app = Flask(__name__)
 app.config.from_object(Config)
+
 if os.environ.get('FP_CONFIG',"DEV") == 'PROD':
     app.config.from_object(ProdConfig)
 else:
     app.config.from_object(DevConfig)
+
+app.debug = app.config['DEBUG']
 api=Api(app)
 
 db = SQLAlchemy(app)
 migrate = Migrate(app,db) 
+ma = Marshmallow(app)
 cors = CORS(app)
-import models
+from models import Bouts,Events,BoutSchema,EventSchema
 
 class PredictionsResource(Resource):
+    """
+    for the predictions module
+    """
     def get(self):
-        print(app.config['HELLO'],file=sys.stderr)
         print(app.config['ROUTES']['basedir'],file=sys.stderr)
         print(app.config['HOST'],file=sys.stderr)
         #subprocess.call(['python3','predictions.py'])
-        with open(app.config['ROUTES']['basedir'] + 'pred_fights.json') as jf:
+        with open(app.config['ROUTES']['basedir'] + '/pred_fights.json') as jf:
             data = json.load(jf)
-        return jsonify(data)
+        return jsonify(data), 200
 
-api.add_resource(PredictionsResource,'/winners')
+
+class EventResouce(Resource):
+    """
+    Get a specfic event
+    """
+    def get(self,event_id):
+        msg = {}
+        bout_schema = BoutSchema()
+        event_schema = EventSchema()
+        print("Recieved " + str(event_id), file=sys.stderr)
+        rows = Bouts.query.filter_by(event_id=str(event_id)).all()
+        event = Events.query.filter_by(id=event_id).first()
+        msg['event'] = event_schema.dump(event)
+        msg['bouts'] = []
+        for bout in rows:
+            #print(bout_schema.dump(bout),file=sys.stderr)
+            msg['bouts'].append(bout_schema.dump(bout))
+        return msg, 200
+
+class EventsResource(Resource):
+    """
+    Get all events with their corresponding id
+    param: all gets all of the events, regardless if there are bouts available for them
+    exist: only gets events that have bouts available
+    """
+    def get(self, param):
+        msg = {}
+        event_schema = EventSchema()
+        msg['events'] = []
+        event_rows = Events.query.filter_by().all()
+        if param == "all":
+            for event in event_rows:
+                msg['events'].append(event_schema.dump(event))
+        elif param == "existing":
+            for event in event_rows:
+                if Bouts.query.filter_by(event_id=event.id).first() != None:
+                    msg['events'].append(event_schema.dump(event))
+        return msg,200
+
+api.add_resource(PredictionsResource,'/predictions')
+api.add_resource(EventResouce,'/event/<int:event_id>')
+api.add_resource(EventsResource,'/events/<string:param>')
 
 class ConfigURL():
     """
@@ -45,5 +93,8 @@ class ConfigURL():
         self.live_url = "whatever"	
         return self.live_url
 
+@app.errorhandler(405)
+def request_not_supported(e):
+    return("this method is unsupported"), 405
 if __name__ == '__main__':
     app.run(host=app.config['HOST'], debug=app.config['DEBUG'])
