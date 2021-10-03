ae4af0b929d98dc3a22baa62e4576b9bb0d21254
mavHo <homaverick@gmail.com>
2021-10-03 16:43:27 -0700

    Updated ufc stats parse module

--- UFC_scraper/UFC_stats_parse.py
+++ UFC_scraper/UFC_stats_parse.py
@@ -1,74 +1,28 @@
 import json, sys, re, time, random
 import urllib.request
-from proxy import Proxy
 from bs4 import BeautifulSoup
 from flask import jsonify
 
+import asyncio
 
-class UFC_Stats_Parser():
+from rotatingProxy.rotatingProxy import RotatingProxy
+
+class UFCWebScraper():
     """
     Web crawler dedicated to the ufc stats events page ;)
     """
 
-    def __init__(self,plistfile):
+    def __init__(self,proxy_path):
         """
         Takes in plistfile, which is the location of the proxy_list.txt
         """
-        self.working_proxy = ''
-        self.proxy_list = []
-        self.plistfile = plistfile
-        self._generateProxyList(self.proxy_list,self.plistfile)
-    
-    def _generateProxyList(self, proxy_list, plist):
-        fobj = open(plist, "r")
-        #fobj = open("proxy_list.txt","r")
-        for line in fobj:
-            self.proxy_list.append(Proxy(line))
+        self.r_proxy = RotatingProxy(proxy_path)
 
 
-    def getRawHTML(self,url):
-        """
-        Takes a URL. Attempts to obtain html data from the webpage.
-        Users a rotating proxy. Returns data in bytes
-        """
-        success=False
-        for proxy in self.proxy_list[:]:
-            proxy.generateHeader() 
-            req = urllib.request.Request(
-                url = url,
-                data = None,
-                headers = proxy.header,
-            )
-            authinfo = urllib.request.HTTPBasicAuthHandler()
-            if self.working_proxy is not '':
-                proxy_support = urllib.request.ProxyHandler({'http': self.working_proxy})
-            else:
-                proxy_support = urllib.request.ProxyHandler({'http': proxy.ip})
-            opener = urllib.request.build_opener(proxy_support,authinfo,urllib.request.CacheFTPHandler)
-            print('opener', flush=True)
+        self.events_all_url = 'http://ufcstats.com/statistics/events/completed?page=all'
 
-            #urllib.request.install_opener(opener)
-            try:
-                endpoint = opener.open(req)
-                #endpoint = urllib.request.urlopen(req) 
-                mybytes = endpoint.read()
-                endpoint.close()
-                print('Able to open ' + proxy.ip,flush=True)
-                self.working_proxy = proxy.ip
-                success=True
-                time.sleep(random.randrange(20))
-                break
-            except Exception as e:
-                success=False
-                print(e ,flush=True)
-                self.working_proxy = ''
-                #proxy_list.remove(proxy)
-                time.sleep(random.randrange(20))
-        #endpoint = request.get(url) #mybytes = endpoint.content
-        return mybytes 
-    
-    #returns a json from the events page of ufc_stats with query set to all.
-    #json contains links and event names
+        self.loop = asyncio.get_event_loop()
+ 
     def generate_url_list(self, data):
         """
         Given html data of the event list on the UFC website,
@@ -88,14 +42,6 @@ class UFC_Stats_Parser():
             #print(listing['href'] + ' ' + listing.get_text(strip=True))
         return result
 
-    def parse_event_list(self, data):
-        """
-        Not used 
-        """
-        soup = BeautifulSoup(data, 'lxml')
-        payload = soup.find('table', {'class', 'b-statistics__table-events'})
-        return payload
-
     def parse_event_fights(self, data):
         """
         Returns a list with all of the fight stats of each bout
@@ -107,10 +53,8 @@ class UFC_Stats_Parser():
         
         fight_list = []
         for listing in payload:
-            fight_stats = {}
-            self._parse_listing(listing, fight_stats)
-            fight_list.append(fight_stats)
-
+            fight_list.append(self._parse_listing(listing))
+           
         return fight_list
     
     def generate_event_bout_list(self,payload):
@@ -133,27 +77,32 @@ class UFC_Stats_Parser():
 
         return fight_list
     
-    def _parse_listing(self, data, fight_stats):
+    def _parse_listing(self, data):
         """
         Parse the contents section of a td. Populates a json called fight_stats
         This looks at main bout info: Time, Round, Method, WeightClass, winner,loser, res
 
         Do not call this outside the file!
         """
-        #print(data['data-link'])
+        fight_stats = {}
         payload = data.find_all('td')
         fight_stats['Time'] = payload[9].get_text(strip=True)
         fight_stats['Round'] = payload[8].get_text(strip=True)
         fight_stats['Method'] = payload[7].get_text(strip=True)
         fight_stats['WeightClass'] = payload[6].get_text(strip=True)
+
+        ### fighter name as string
         fighter_1 = payload[1].contents[1].get_text(strip=True)
         fighter_2 = payload[1].contents[3].get_text(strip=True)
+
         striking_json = {}
         striking_json2 = {}
         #now we populate striking,td,sub statistics
         fight_details_url = data['data-link']
-        data_bytes = self.getRawHTML(fight_details_url)
-        self._parse_striking_stats(data_bytes,fight_stats,striking_json,striking_json2) 
+        data_bytes = self.r_proxy.getRawHTML(fight_details_url)
+        self._parse_striking_stats(data_bytes,fight_stats) 
+
+
         result = payload[0].get_text(strip=True)
 
         if result == 'win':
@@ -165,47 +114,53 @@ class UFC_Stats_Parser():
             fight_stats['Loser'] = 'NA' 
             fight_stats['Result'] = result
 
+        return fight_stats
+
     #given fight details, parse bout statistics    
     #New link so have to open another soup obj
-    def _parse_striking_stats(self, data,fight_stats,fighter1_stats, fighter2_stats):
+    def _parse_striking_stats(self, data,fight_stats):
         """
         Parses striking statistics. Populates two json's
         """
+        red_stats = {}
+        blue_stats = {}
+
         soup = BeautifulSoup(data, 'lxml')
         table = soup.find('table')
         columns = table.find_all('td')
+
         red_fighter = columns[0].contents[1].get_text(strip=True)
         blue_fighter = columns[0].contents[3].get_text(strip=True)
 
-        fighter1_stats['fighter'] = red_fighter 
-        fighter2_stats['fighter'] = blue_fighter        
+        red_stats['fighter'] = red_fighter 
+        blue_stats['fighter'] = blue_fighter        
 
-        fighter1_stats['KD'] = columns[1].contents[1].get_text(strip=True)
-        fighter2_stats['KD'] = columns[1].contents[3].get_text(strip=True)
+        red_stats['KD'] = columns[1].contents[1].get_text(strip=True)
+        blue_stats['KD'] = columns[1].contents[3].get_text(strip=True)
 
-        fighter1_stats['SIGSTR'] = re.sub(' of ', '/', columns[2].contents[1].get_text(strip=True))
-        fighter2_stats['SIGSTR'] = re.sub(' of ', '/', columns[2].contents[3].get_text(strip=True))
+        red_stats['SIGSTR'] = re.sub(' of ', '/', columns[2].contents[1].get_text(strip=True))
+        blue_stats['SIGSTR'] = re.sub(' of ', '/', columns[2].contents[3].get_text(strip=True))
 
-        fighter1_stats['SIGSTR_PRCT'] = columns[3].contents[1].get_text(strip=True)
-        fighter2_stats['SIGSTR_PRCT'] = columns[3].contents[3].get_text(strip=True)
+        red_stats['SIGSTR_PRCT'] = columns[3].contents[1].get_text(strip=True)
+        blue_stats['SIGSTR_PRCT'] = columns[3].contents[3].get_text(strip=True)
 
-        fighter1_stats['TTLSTR'] = re.sub(' of ', '/', columns[4].contents[1].get_text(strip=True))
-        fighter2_stats['TTLSTR'] = re.sub(' of ', '/', columns[4].contents[3].get_text(strip=True))
+        red_stats['TTLSTR'] = re.sub(' of ', '/', columns[4].contents[1].get_text(strip=True))
+        blue_stats['TTLSTR'] = re.sub(' of ', '/', columns[4].contents[3].get_text(strip=True))
 
-        fighter1_stats['TD'] = re.sub(' of ', '/', columns[5].contents[1].get_text(strip=True))
-        fighter2_stats['TD'] = re.sub(' of ', '/',columns[5].contents[3].get_text(strip=True))
+        red_stats['TD'] = re.sub(' of ', '/', columns[5].contents[1].get_text(strip=True))
+        blue_stats['TD'] = re.sub(' of ', '/',columns[5].contents[3].get_text(strip=True))
 
-        fighter1_stats['TD_PRCT'] = columns[6].contents[1].get_text(strip=True)
-        fighter2_stats['TD_PRCT'] = columns[6].contents[3].get_text(strip=True)
+        red_stats['TD_PRCT'] = columns[6].contents[1].get_text(strip=True)
+        blue_stats['TD_PRCT'] = columns[6].contents[3].get_text(strip=True)
 
-        fighter1_stats['SUB'] = columns[7].contents[1].get_text(strip=True)
-        fighter2_stats['SUB'] = columns[7].contents[3].get_text(strip=True)
+        red_stats['SUB'] = columns[7].contents[1].get_text(strip=True)
+        blue_stats['SUB'] = columns[7].contents[3].get_text(strip=True)
 
-        fighter1_stats['PASS'] = columns[8].contents[1].get_text(strip=True)
-        fighter2_stats['PASS'] = columns[8].contents[3].get_text(strip=True)
+        red_stats['PASS'] = columns[8].contents[1].get_text(strip=True)
+        blue_stats['PASS'] = columns[8].contents[3].get_text(strip=True)
 
-        fight_stats['Red'] = fighter1_stats
-        fight_stats['Blue'] = fighter2_stats
+        fight_stats['Red'] = red_stats
+        fight_stats['Blue'] = blue_stats
 
 def main():
     #url = 'http://www.ufcstats.com/statistics/events/completed?page=all'
