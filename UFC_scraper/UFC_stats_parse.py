47d07843edff2c2e61b7a9fea06c5fc34ccf4c0c
Maverick <homaverick@gmail.com>
2022-01-17 18:00:37 +0000

    Updated to use rotating proxy, and updated to run async. Currently only works to handle the events page

--- UFC_scraper/UFC_stats_parse.py
+++ UFC_scraper/UFC_stats_parse.py
@@ -3,43 +3,155 @@ import urllib.request
 from bs4 import BeautifulSoup
 from flask import jsonify
 
+from dataclasses import dataclass 
+from typing import Set, Iterable, List, Tuple, Dict, Optional
+
 import asyncio
 
 from rotatingProxy.rotatingProxy import RotatingProxy
+
+@dataclass
+class TaskQueueMessage:
+    url: str
+    depth: int
+    retry_count: int
 
 class UFCWebScraper():
     """
     Web crawler dedicated to the ufc stats events page ;)
     """
 
-    def __init__(self,proxy_path):
+    def __init__(self):
         """
         Takes in plistfile, which is the location of the proxy_list.txt
         """
-        self.r_proxy = RotatingProxy(proxy_path)
-
+        self.r_proxy = RotatingProxy()
 
         self.events_all_url = 'http://ufcstats.com/statistics/events/completed?page=all'
 
+        self.crawled_urls = set() 
+
+        self.concurrency = 20
+
+        self.results = []
+
         self.loop = asyncio.get_event_loop()
+
  
-    def generate_url_list(self, data):
+    async def crawl(self) -> None:
+        ### crawl through the links.
+
+        ### download the bytes needed to process the listing, 
+        ### submit the task to worker threads, where they then process with beautiful soup, and commit to the DB.
+
+        self.task_queue = asyncio.Queue()
+
+        task_message = TaskQueueMessage(self.events_all_url, 0, 0)
+
+        self.task_queue.put_nowait(task_message)
+
+        print("Starting workers")
+        workers = [asyncio.create_task(self.worker()) for i in range(self.concurrency)]
+
+        print("Working")
+        await self.task_queue.join()
+
+        for w in workers:
+            w.cancel()
+        print("Finished")
+            
+        if self.r_proxy.session:
+            await self.r_proxy.session.close()
+
+    async def worker(self):
+        """
+        Gets a url from the task queue, and then crawls on the page.
+        """
+        while True:
+
+            ### if queue is empty, leave
+            if not self.task_queue:
+                break
+
+            ### get task
+            task = await self.task_queue.get()
+            
+            ### If we've already seen this url, skip it. 
+            if task.url in self.crawled_urls:
+                self.task_queue.task_done()
+                continue
+                
+            self.crawled_urls.add(task.url)
+
+            ### crawl the page.
+            try:
+                url,links,html = await self.crawl_page(task.url,task.depth)
+            except Exception as e:
+                print("Ran into Exception")
+                print(e)
+            else:
+                #self.results.append(self.parse(url,links,html))
+                if links:
+                    self.results.append(links)
+
+                ### insert new links into the queue.
+                for link in links.difference(self.crawled_urls):
+                    task_message = TaskQueueMessage(link, task.depth + 1, 0)
+                    await self.task_queue.put(task_message)
+            finally:
+                self.task_queue.task_done()
+
+    async def crawl_page(self,url:str,depth:int) -> Tuple[str,Set[str],str]:
+        """
+        Grabs a webpage and returns all data from it.
+
+        Args:
+            url (str): url as a string.
+
+            depth (int): current depth we're at. Depth will also specify how we parse the html.
+        Returns:
+            Tuple[str,Set[str],str]: [description]
+        """
+        #print(url,depth)
+        ### Depth 0 -> events_all_url page. What we do here is generate the url list.
+        ### Depth 1 -> An events page with all the bouts.
+        ### Depth 2 -> a Bouts page.
+
+        if depth == 0:
+            html = await self.r_proxy._make_request(url)
+            links = self.generate_url_list(html)
+        else:
+            html = ''
+            links = set()
+
+        return url,links,html
+
+    async def get_results(self) -> List:
+        await self.crawl()
+        
+        return self.results
+
+
+
+        
+    def generate_url_list(self, data:str) -> Set[str]:
         """
         Given html data of the event list on the UFC website,
-        generates a URL list of all the events and all their urls
+
+        generates a URL set of all the events and all their urls
         """
         soup = BeautifulSoup(data,'lxml') 
         payload = soup.find('table', {'class', 'b-statistics__table-events'})
         payload = payload.find_all('a', href=True)
 
-        result = []
+        result = set() 
         for listing in payload:
-            row = {}
+            #row = {}
             #listing['href']
-            row['event'] = listing.get_text(strip=True)
-            row['link'] = listing['href']
-            result.append(row)
-            #print(listing['href'] + ' ' + listing.get_text(strip=True))
+            #row['event'] = listing.get_text(strip=True)
+            #row['link'] = listing['href']
+            result.add(listing['href'])
+
         return result
 
     def parse_event_fights(self, data):
@@ -161,22 +273,3 @@ class UFCWebScraper():
 
         fight_stats['Red'] = red_stats
         fight_stats['Blue'] = blue_stats
-
-def main():
-    #url = 'http://www.ufcstats.com/statistics/events/completed?page=all'
-    url = 'http://http://www.ufcstats.com/statistics/events/completed'
-    parser = UFC_Stats_Parser()
-    print('Get current fights test------------------')    
-
-
-    #url = 'http://www.ufcstats.com/event-details/94a5aaf573f780ad'
-    data_bytes = parser.getRawHTML(url)
-    url_list = parser.generate_url_list(data_bytes)
-    fight_list=parser.generate_event_bout_list(url_list[0])
-    print(fight_list)
-
-
-
-
-if __name__ == '__main__':
-    main()
