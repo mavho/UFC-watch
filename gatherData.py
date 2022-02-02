c1e7ac1aef37caaf263dc2536b93066dee90e0bf
Maverick <homaverick@gmail.com>
2022-02-02 04:13:15 +0000

    Scrapping upcoming and past event added to script. Prediction module is then run on the upcoming events.

--- gatherData.py
+++ gatherData.py
@@ -1,10 +1,9 @@
 #This py file is for grabbing the ufcstats with the webscraper, then populating a db.
-#Then we run the predictions module.
+#Then we run the predictions module to write the predictions out.
 
 from pprint import pprint
 from UFC_scraper.UFC_stats_parse import UFCWebScraper 
-from predictions import predictions
-import asyncio, os
+import asyncio, os, json
 import UFC_scraper.db_helper as dh
 from predictions.predictions import Predictions
 from sqlalchemy import create_engine
@@ -15,16 +14,17 @@ from ufc_api import db
 
 
 def main():
+    ### Get the latest UFC event (that just finished) and add it to the DB.
     scraper = UFCWebScraper()
-    #latest_event = asyncio.run(scraper.get_results(start=2,end=3))
-#    dh.populate_bouts_fighters_table(latest_event)
- #   print("Finished latest event")
-#    new_scapper = UFCWebScraper()
+    latest_event = asyncio.run(scraper.get_results(start=2,end=3))
+    dh.populate_bouts_fighters_table(latest_event)
+    print("Finished latest event")
+    ### Grab the upcoming event
     upcoming_event = asyncio.run(scraper.get_results(start=1,end=2))
     print("Finished upcoming event")
     path = os.getcwd()
 
-    ### create boutlist for this week
+    ### create boutlist for this week with upcoming event data.
     with open(path +'/bout_list.txt','w') as fopen:
         for e_link,event_data in upcoming_event.items():
             fopen.write(event_data['name']+'\n')
@@ -34,12 +34,12 @@ def main():
     db_engine = create_engine(Config.SQLALCHEMY_DATABASE_URI)
     ### Predict new boutlist.
     pm = Predictions(db_engine)
-    res = pm.predict()
-    pprint(res)
+    pred_json = pm.predict()
 
-
-### run the predictions module too.
-
+    #### Write results to pred fights json.
+    json_obj = json.dumps(pred_json)
+    with open(path + "/pred_fights.json","w") as fopen:
+        fopen.write(json_obj)
 
 if __name__ == '__main__':
     main()
